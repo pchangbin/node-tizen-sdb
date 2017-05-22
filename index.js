66a1ae975ba7902e60b2c306b8284b20fee0fa20
Changbin Park <changbin.park@intel.com>
2017-05-22 16:14:29 +0900

    sdb.root and sdb.shell is implemented

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/* global module, child_process, os */
+const {execFile, spawnSync} = child_process;
+
+const execName = 'sdb';
+(function checkSDBAvailable() {
+  const whereis = os.platform().includes('win') ? 'where' : 'which';
+  if (spawnSync(whereis, [execName]).status !== 0) {
+    throw new Error(`SDB command ${execName} is not available in PATH.`);
+  }
+})();
+
+module.exports = {
+  root(aOn = true) {
+    const param = (function() {
+      try { aOn = aOn.toLowerCase() !== 'off'; } catch(e) { }
+
+      return aOn ? 'on' : 'off';
+    })();
+    execFile(execName, ['root', param], showResult);
+    return this;
+  },
+  shell(aCmd) {
+    execFile(execName, ['shell', aCmd], showResult);
+  }
+};
+
+const isRequiredInRepl = module.parent.id === '<repl>';
+function showResult(aError, aStdout, aStderr) {
+  if (!isRequiredInRepl) return;
+
+  if (aError) throw new Error(aError);
+  if (aStdout) console.log(aStdout);
+  if (aStderr) console.error(aStderr);
+}
+
